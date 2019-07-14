7503dd39a65ed60705825c8d338357e6cb1765e2
RJ Pablo <numerianop@sedata.com>
2019-07-15 02:12:21 +0800

    feat: Add support for OAuth Bearer Tokens Generation

---
+++ DribblyAuthAPI/Providers/SimpleAuthorizationServerProvider.cs
@@ -0,0 +1,55 @@
+﻿using DribblyAuthAPI.Repositories;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin.Security.OAuth;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace DribblyAuthAPI.Providers
+{
+    public class SimpleAuthorizationServerProvider : OAuthAuthorizationServerProvider
+    {
+        /// <summary>
+        /// Responsible for validating the client
+        /// </summary>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        public override async Task ValidateClientAuthentication(OAuthValidateClientAuthenticationContext context)
+        {
+            context.Validated();
+        }
+
+        /// <summary>
+        /// Responsible to validate the username and password when logging in
+        /// </summary>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        public override async Task GrantResourceOwnerCredentials(OAuthGrantResourceOwnerCredentialsContext context)
+        {
+
+            //check if the username-passowrd combination exists
+            using (AuthRepository _repo = new AuthRepository())
+            {
+                IdentityUser user = await _repo.FindUser(context.UserName, context.Password);
+
+                if (user == null)
+                {
+                    context.SetError("invalid_grant", "The user name or password is incorrect.");
+                    return;
+                }
+            }
+
+            var identity = new ClaimsIdentity(context.Options.AuthenticationType);
+            //The following info will be added to the token. You can add different claims here
+            //but the token size will increase for sure
+            identity.AddClaim(new Claim("sub", context.UserName));
+            identity.AddClaim(new Claim("role", "user"));
+
+            context.Validated(identity);
+
+        }
+    }
+}
--- DribblyAuthAPI/Startup.cs
+++ DribblyAuthAPI/Startup.cs
@@ -1,6 +1,8 @@
 ﻿//reference: http://bitoftech.net/2014/06/01/token-based-authentication-asp-net-web-api-2-owin-asp-net-identity/
 using Microsoft.Owin;
+using Microsoft.Owin.Security.OAuth;
 using Owin;
+using System;
 using System.Web.Http;
 
 [assembly: OwinStartup(typeof(DribblyAuthAPI.API.Startup))]
@@ -18,6 +20,7 @@ namespace DribblyAuthAPI.API
         /// <param name="app"></param>
         public void Configuration(IAppBuilder app)
         {
+            ConfigureOAuth(app);
             //The “HttpConfiguration” object is used to configure API routes, so we’ll pass this object
             //to method “Register” in “WebApiConfig” class.
             HttpConfiguration config = new HttpConfiguration();
@@ -25,6 +28,22 @@ namespace DribblyAuthAPI.API
             //we’ll pass the “config” object to the extension method “UseWebApi” which will be responsible
             //to wire up ASP.NET Web API to our Owin server pipeline
             app.UseWebApi(config);
+        }
+
+        public void ConfigureOAuth(IAppBuilder app)
+        {
+            OAuthAuthorizationServerOptions OAuthServerOptions = new OAuthAuthorizationServerOptions()
+            {
+                AllowInsecureHttp = true,
+                TokenEndpointPath = new PathString("/token"), //http://localhost:[port]/token
+                AccessTokenExpireTimeSpan = TimeSpan.FromDays(1), //set token validatity to 24 hours
+                Provider = new SimpleAuthorizationServerProvider() //specifies implementation on how to validate the credentials for users asking for tokens
+            };
+
+            // Token Generation
+            app.UseOAuthAuthorizationServer(OAuthServerOptions);
+            app.UseOAuthBearerAuthentication(new OAuthBearerAuthenticationOptions());
+
         }
 
     }
