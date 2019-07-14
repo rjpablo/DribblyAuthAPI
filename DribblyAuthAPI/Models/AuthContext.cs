0433d8dd04ada5e47a78714e81f382e482abccaf
RJ Pablo <numerianop@sedata.com>
2019-07-15 00:55:32 +0800

    setup: add DB context and connection string

---
+++ DribblyAuthAPI/Models/AuthContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace DribblyAuthAPI.Models
+{
+    //This serves as our main connection to our database and Identity tables
+    //All Identity tranactions will run on this context
+    //You can think about IdentityDbContext class as special version of the traditional “DbContext” Class
+    //responsible for handling Identity transactions
+    public class AuthContext : IdentityDbContext<IdentityUser>
+    {
+        public AuthContext()
+            : base("AuthContext")
+        {
+
+        }
+    }
+}
