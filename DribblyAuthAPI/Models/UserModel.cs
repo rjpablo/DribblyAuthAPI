7a7c5a123f411cd8348f0e373312bae1a235f921
RJ Pablo <numerianop@sedata.com>
2019-07-15 01:41:46 +0800

    feat: add UserModel class and AuthRepository.cs

---
+++ DribblyAuthAPI/Models/UserModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DribblyAuthAPI.Models
+{
+    public class UserModel
+    {
+        [Required]
+        public string UserName { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
+    }
+}
