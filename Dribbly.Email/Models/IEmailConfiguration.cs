990d94770a330c80c1835c5802d9833ca0e9dc7e
Rj Pablo <numerianop@sedata.com>
2020-04-04 19:10:53 +0800

    feat: WEB-0066 - add Email Service

---
+++ Dribbly.Email/Models/IEmailConfiguration.cs
@@ -0,0 +1,11 @@
+﻿namespace Dribbly.Email.Models
+{
+    public interface IEmailConfiguration
+    {
+        string From { get; set; }
+        string SmtpServer { get; set; }
+        int Port { get; set; }
+        string UserName { get; set; }
+        string Password { get; set; }
+    }
+}
---
+++ Dribbly.Email/Services/EmailService.cs
@@ -0,0 +1,60 @@
+﻿using Dribbly.Email.Models;
+using MailKit.Net.Smtp;
+using MimeKit;
+using System.Threading.Tasks;
+
+namespace Dribbly.Email.Services
+{
+    public class EmailService: IEmailService
+    {
+        private readonly IEmailConfiguration _emailConfig;
+
+        public EmailService(IEmailConfiguration emainConfig)
+        {
+            _emailConfig = emainConfig;
+        }
+
+        public async Task SendEmailAsync(EmailMessage message)
+        {
+            var mailMessage = CreateEmailMessage(message);
+
+            await SendAsync(mailMessage);
+        }
+
+        private MimeMessage CreateEmailMessage(EmailMessage message)
+        {
+            var emailMessage = new MimeMessage();
+            emailMessage.From.Add(new MailboxAddress(_emailConfig.From));
+            emailMessage.To.AddRange(message.To);
+            emailMessage.Subject = message.Subject;
+            emailMessage.Body = new TextPart(MimeKit.Text.TextFormat.Html) { Text = message.Content };
+
+            return emailMessage;
+        }
+
+        private async Task SendAsync(MimeMessage mailMessage)
+        {
+            using (var client = new SmtpClient())
+            {
+                try
+                {
+                    await client.ConnectAsync(_emailConfig.SmtpServer, _emailConfig.Port, true);
+                    client.AuthenticationMechanisms.Remove("XOAUTH2");
+                    await client.AuthenticateAsync(_emailConfig.UserName, _emailConfig.Password);
+
+                    await client.SendAsync(mailMessage);
+                }
+                catch
+                {
+                    //log an error message or throw an exception or both.
+                    throw;
+                }
+                finally
+                {
+                    await client.DisconnectAsync(true);
+                    client.Dispose();
+                }
+            }
+        }
+    }
+}
