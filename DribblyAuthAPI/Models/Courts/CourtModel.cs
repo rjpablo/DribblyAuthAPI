6b42a90eb64bf3436336141fbf36d759bafd191b
Rj Pablo <numerianop@sedata.com>
2020-05-25 05:41:57 +0800

    feat: WEB-0079 - Add court sorting

--- DribblyAuthAPI/Models/Courts/CourtModel.cs
+++ DribblyAuthAPI/Models/Courts/CourtModel.cs
@@ -16,7 +16,7 @@ namespace DribblyAuthAPI.Models.Courts
         [Required, MaxLength(100)]
         public string Name { get; set; }
 
-        public decimal? RatePerHour { get; set; }
+        public decimal RatePerHour { get; set; }
 
         public decimal? Rating { get; set; }
 
