8a888d392e5756eb740a7498c93a2aa5d6c52e3f
rjpablo <rjpablo.secondary@gmail.com>
2023-07-31 19:11:42 +0800

    HOOP-137: add BaseStatsSummaryModel.cs

---
+++ Dribbly.Model/Entities/BaseStatsSummaryModel.cs
@@ -0,0 +1,50 @@
+﻿using Dribbly.Core.Models;
+
+namespace Dribbly.Model.Entities
+{
+    public class BaseStatsSummaryModel : BaseEntityModel
+    {
+        /// <summary>
+        /// Games played
+        /// </summary>
+        public int GP { get; set; }
+        /// <summary>
+        /// Games Won
+        /// </summary>
+        public int GW { get; set; }
+        /// <summary>
+        /// Points per game
+        /// </summary>
+        public double PPG { get; set; }
+        /// <summary>
+        /// Rebounds per game
+        /// </summary>
+        public double RPG { get; set; }
+        /// <summary>
+        /// Assists per game
+        /// </summary>
+        public double APG { get; set; }
+        /// <summary>
+        /// Field Goal Percentage
+        /// </summary>
+        public double FGP { get; set; }
+        /// <summary>
+        /// Blocks per game
+        /// </summary>
+        public double BPG { get; set; }
+        /// <summary>
+        /// Turnovers per game
+        /// </summary>
+        public int TPG { get; set; }
+        /// <summary>
+        /// 3-point percentage
+        /// </summary>
+
+        /// <summary>
+        /// Steals per game
+        /// </summary>
+        public int SPG { get; set; }
+        public double ThreePP { get; set; }
+        public double OverallScore { get; set; }
+    }
+}
--- Dribbly.Model/Entities/BaseTeamStatsModel.cs
+++ Dribbly.Model/Entities/BaseTeamStatsModel.cs
@@ -4,53 +4,11 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Dribbly.Model.Entities
 {
-    public abstract class BaseTeamStatsModel : BaseEntityModel
+    public abstract class BaseTeamStatsModel : BaseStatsSummaryModel
     {
         [ForeignKey(nameof(Team))]
         public long TeamId { get; set; }
 
         public TeamModel Team { get; set; }
-        /// <summary>
-        /// Games played
-        /// </summary>
-        public int GP { get; set; }
-        /// <summary>
-        /// Games Won
-        /// </summary>
-        public int GW { get; set; }
-        /// <summary>
-        /// Points per game
-        /// </summary>
-        public double PPG { get; set; }
-        /// <summary>
-        /// Rebounds per game
-        /// </summary>
-        public double RPG { get; set; }
-        /// <summary>
-        /// Assists per game
-        /// </summary>
-        public double APG { get; set; }
-        /// <summary>
-        /// Field Goal Percentage
-        /// </summary>
-        public double FGP { get; set; }
-        /// <summary>
-        /// Blocks per game
-        /// </summary>
-        public double BPG { get; set; }
-        /// <summary>
-        /// Turnovers per game
-        /// </summary>
-        public int TPG { get; set; }
-        /// <summary>
-        /// 3-point percentage
-        /// </summary>
-
-        /// <summary>
-        /// Steals per game
-        /// </summary>
-        public int SPG { get; set; }
-        public double ThreePP { get; set; }
-        public double OverallScore { get; set; }
     }
 }
