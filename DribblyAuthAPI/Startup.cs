dec1b7f6c0aebbe8d408d88d856e316d79cd9864
rjpablo <rjpablo.secondary@gmail.com>
2023-10-10 08:47:43 +0800

    HOOP-276: extens auth token lifespan to 3 hours

--- DribblyAuthAPI/Startup.cs
+++ DribblyAuthAPI/Startup.cs
@@ -89,7 +89,7 @@ namespace DribblyAuthAPI.API
             {
                 AllowInsecureHttp = true,
                 TokenEndpointPath = new PathString("/token"), //http://localhost:[port]/token
-                AccessTokenExpireTimeSpan = TimeSpan.FromMinutes(30), //set token validatity to 30 minutes
+                AccessTokenExpireTimeSpan = TimeSpan.FromHours(3), //set token validatity to 30 minutes
                 Provider = new SimpleAuthorizationServerProvider(), //specifies implementation on how to validate the credentials for users asking for tokens
                 RefreshTokenProvider = new SimpleRefreshTokenProvider()
             };
